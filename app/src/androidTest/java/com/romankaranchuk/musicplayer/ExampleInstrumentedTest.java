33fee6be635b15401468286a38c553b2d5287a08
inv779 <109506977+inv779@users.noreply.github.com>
2020-12-22 14:12:23 +0000

    resolving conflicts

--- app/src/androidTest/java/com/romankaranchuk/musicplayer/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/romankaranchuk/musicplayer/ExampleInstrumentedTest.java
@@ -5,4 +5,10 @@ import static org.junit.Assert.assertEquals;
 import android.content.Context;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.platform.app.Instrume
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Instrumentation test, which will execute on an Android devic

9ea90361125c9152f86515cdacde555858ce9959
inv779 <109506977+inv779@users.noreply.github.com>
2020-12-25 05:35:31 +0000

    Translation files has been updated

--- app/src/androidTest/java/com/romankaranchuk/musicplayer/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/romankaranchuk/musicplayer/ExampleInstrumentedTest.java
@@ -17,4 +17,7 @@ import org.junit.runner.RunWith;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistr

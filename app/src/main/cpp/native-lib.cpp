b9a9735439ed8bda5187684b6b05c706bbfbd372
inv779 <109506977+inv779@users.noreply.github.com>
2020-12-28 19:27:52 +0000

    utehn-smartcare

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -40,3 +40,34 @@ Java_com_romankaranchuk_musicplayer_utils_JniUtils_sum(
 
     std::clock_t start_time = std::clock();
     long long sum = 0;
+    for (int j = 0; j < 10000; ++j){
+        for (int i = 0; i < vector.size(); ++i){
+            sum += vector[i];
+        }
+    }
+    long running_time = (long) ((std::clock() - start_time) / (double) (CLOCKS_PER_SEC / 1000));
+    __android_log_print(ANDROID_LOG_DEBUG, "JNI running time", "%ld", running_time);
+
+    return sum;
+}
+
+void init(JNIEnv* envIn){
+    env = envIn;
+    java_util_ArrayList         = static_cast<jclass>(env->NewGlobalRef(env->FindClass("java/util/ArrayList")));
+    java_util_ArrayList_size    = env->GetMethodID(java_util_ArrayList, "size", "()I");
+    java_util_ArrayList_get     = env->GetMethodID(java_util_ArrayList, "get", "(I)Ljava/lang/Object;");
+    java_lang_Integer           = static_cast<jclass>(env->NewGlobalRef(env->FindClass("java/lang/Integer")));
+    java_lang_Integer_intValue  = env->GetMethodID(java_lang_Integer, "intValue", "()I");
+}
+
+std::vector<int> arrayListToVector(jobject arrayList){
+    unsigned long len = (unsigned long) env->CallIntMethod(arrayList, java_util_ArrayList_size);
+    std::vector<int> result;
+    result.reserve(len);
+    for (jint i = 0; i < len; i++){
+        jobject element = env->CallObjectMethod(arrayList, java_util_ArrayList_get, i);
+        int value = env->CallIntMethod(element, java_lang_Integer_intValue);
+        result.push_back(value);
+    }
+    return result;
+}
---
+++ app/src/main/java/com/romankaranchuk/musicplayer/common/MusicPlayer.kt
@@ -0,0 +1,6 @@
+package com.romankaranchuk.musicplayer.common
+
+import android.media.MediaPlayer
+import java.io.IOException
+
+interface MusicPlay

5203a23fddc5858f650c64a052a4227a1d5276e7
inv779 <109506977+inv779@users.noreply.github.com>
2020-12-29 14:06:58 +0000

    Fix typos in documentation

--- app/src/main/java/com/romankaranchuk/musicplayer/common/MusicPlayer.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/common/MusicPlayer.kt
@@ -3,4 +3,10 @@ package com.romankaranchuk.musicplayer.common
 import android.media.MediaPlayer
 import java.io.IOException
 
-interface MusicPlay
+interface MusicPlayer {
+    fun resume()
+    fun pause()
+    fun prepare(filepath: String)
+    fun start(filepath: String)
+    fun stop()
+    fun repeat

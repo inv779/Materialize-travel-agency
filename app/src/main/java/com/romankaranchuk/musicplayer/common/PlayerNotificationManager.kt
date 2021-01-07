d08abbf59146fa3eb73f7f81a9f31dd2d8ba4849
inv779 <109506977+inv779@users.noreply.github.com>
2021-01-07 17:06:42 +0000

    Saved by TiddlyWiki

--- app/src/main/java/com/romankaranchuk/musicplayer/common/PlayerNotificationManager.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/common/PlayerNotificationManager.kt
@@ -1,3 +1,9 @@
 package com.romankaranchuk.musicplayer.common
 
-impor
+import android.app.NotificationManager
+import android.content.Context
+import androidx.core.app.NotificationCompat
+
+interface IPlayerNotificationManager
+
+clas

e576ed57f4ad5014a6f3cd6210fa207cb05d3f9e
inv779 <109506977+inv779@users.noreply.github.com>
2021-01-08 21:48:02 +0000

    em

--- app/src/main/java/com/romankaranchuk/musicplayer/data/Album.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/data/Album.kt
@@ -6,3 +6,38 @@ import androidx.room.PrimaryKey
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import kotlinx.android.parcel.Parcelize
+import java.util.*
+
+@Entity(tableName = "albums")
+@Parcelize
+data class Album (
+    @PrimaryKey
+    @ColumnInfo(name = "album_id")
+    var id: String,
+
+    @ColumnInfo(name = "album_name")
+    var name: String?,
+
+    @ColumnInfo(name = "album_artist")
+    var artist: String?,
+
+    @ColumnInfo(name = "album_path")
+    var path: String?,
+
+    @ColumnInfo(name = "album_image")
+    var imagePath: String?,
+) : Parcelable {
+
+    constructor(
+        name: String,
+        artist: String,
+        path: String,
+        imagePath: String
+    ) : this(
+        id = UUID.randomUUID().toString(),
+        name = name,
+        artist = artist,
+        path = path,
+        imagePath = imagePath
+    )
+}
---
+++ app/src/main/java/com/romankaranchuk/musicplayer/data/Song.kt
@@ -0,0 +1,5 @@
+package com.romankaranchuk.musicplayer.data
+
+import android.os.Parcelable
+import androidx.annotation.NonNull
+impor

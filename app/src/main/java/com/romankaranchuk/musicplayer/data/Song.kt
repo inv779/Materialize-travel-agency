8d2a8b53d3e0dfc7284421068cac4fe9f2539394
inv779 <109506977+inv779@users.noreply.github.com>
2021-01-09 12:28:39 +0000

    Updating homepage

--- app/src/main/java/com/romankaranchuk/musicplayer/data/Song.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/data/Song.kt
@@ -2,4 +2,8 @@ package com.romankaranchuk.musicplayer.data
 
 import android.os.Parcelable
 import androidx.annotation.NonNull
-impor
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.romankaranchuk.musicplayer.utils.MusicUtils
+import kotlinx.and

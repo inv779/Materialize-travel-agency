a86b0168bc18c454825a2e8a00760776b1efcb2a
inv779 <109506977+inv779@users.noreply.github.com>
2021-01-16 06:29:46 +0000

    Tidy.

--- app/src/main/java/com/romankaranchuk/musicplayer/data/db/AppDatabase.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/data/db/AppDatabase.kt
@@ -2,4 +2,10 @@ package com.romankaranchuk.musicplayer.data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.r
+import com.romankaranchuk.musicplayer.data.Album
+import com.romankaranchuk.musicplayer.data.Song
+
+@Database(entities = [
+    Song::class,
+    Album::class
+], v

a03bfed9496ec0ac2d8e81f5b937e998f34d4ce9
inv779 <109506977+inv779@users.noreply.github.com>
2021-03-10 16:20:29 +0000

    Added additional logging

--- app/src/main/java/com/romankaranchuk/musicplayer/data/db/LocalSongDataSource.java
+++ app/src/main/java/com/romankaranchuk/musicplayer/data/db/LocalSongDataSource.java
@@ -77,3 +77,137 @@ public class LocalSongDataSource implements ILocalSongDataSource {
     @Override
     public void deleteSong(@NonNull Song song) {
         songDao.deleteSong(song);
+
+//        SupportSQLiteDatabase db = mDbHelper.getWritableDatabase();
+//        String selection = SongEntry.COLUMN_NAME_ENTRY_ID + " LIKE ?";
+//        String[] selectionArgs = {song.getId()};
+//        db.delete(SongEntry.TABLE_NAME, selection, selectionArgs);
+//        try {
+//            db.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+    }
+
+    @NonNull
+    @Override
+    public List<Song> getSongs(@NonNull String albumId, boolean sortByName) {
+        return songDao.getSongs(albumId, sortByName);
+
+//        SupportSQLiteDatabase db = mDbHelper.getReadableDatabase();
+//        List<Song> songs = new ArrayList<>();
+//        String[] projection = {
+//                SongEntry.COLUMN_NAME_ENTRY_ID,
+//                SongEntry.COLUMN_NAME_SONG_NAME,
+//                SongEntry.COLUMN_NAME_SONG_PATH,
+//                SongEntry.COLUMN_NAME_SONG_DURATION,
+//                SongEntry.COLUMN_NAME_SONG_IMAGE,
+//                SongEntry.COLUMN_NAME_SONG_LYRICS,
+//                SongEntry.COLUMN_NAME_SONG_YEAR,
+//                SongEntry.COLUMN_NAME_SONG_DATE,
+//                SongEntry.COLUMN_NAME_SONG_LANGUAGE
+//        };
+//
+//        String selection = SongEntry.COLUMN_NAME_ALBUM_ID + " LIKE ?";
+//        String[] selectionArgs = {albumId};
+//
+//        String sortBy;
+//        if (sortByName){
+//            sortBy = SongEntry.COLUMN_NAME_SONG_NAME;
+//        } else {
+//            sortBy = SongEntry.COLUMN_NAME_SONG_DURATION;
+//        }
+//
+//        SupportSQLiteQuery query = SupportSQLiteQueryBuilder.builder(SongEntry.TABLE_NAME)
+//                .columns(projection)
+//                .selection(selection, selectionArgs)
+//                .orderBy(sortBy)
+//                .create();
+//        Cursor c = db.query(query);//db.query(SongEntry.TABLE_NAME, projection, selection, selectionArgs, null, null, sortBy);
+//
+//        if (c != null && c.getCount() > 0){
+//            while (c.moveToNext()){
+//                String songId = c.getString(c.getColumnIndexOrThrow(SongEntry.COLUMN_NAME_ENTRY_ID));
+//                String songName = c.getString(c.getColumnIndexOrThrow(SongEntry.COLUMN_NAME_SONG_NAME));
+//                String songPath = c.getString(c.getColumnIndexOrThrow(SongEntry.COLUMN_NAME_SONG_PATH));
+//                int songDuration = c.getInt(c.getColumnIndexOrThrow(SongEntry.COLUMN_NAME_SONG_DURATION));
+//                String songImagePath = c.getString(c.getColumnIndexOrThrow(SongEntry.COLUMN_NAME_SONG_IMAGE));
+//                String songLyrics = c.getString(c.getColumnIndexOrThrow(SongEntry.COLUMN_NAME_SONG_LYRICS));
+//                String songYear = c.getString(c.getColumnIndexOrThrow(SongEntry.COLUMN_NAME_SONG_YEAR));
+//                String songDate = c.getString(c.getColumnIndexOrThrow(SongEntry.COLUMN_NAME_SONG_DATE));
+//                String songLanguage = c.getString(c.getColumnIndexOrThrow(SongEntry.COLUMN_NAME_SONG_LANGUAGE));
+//
+//                if (songId == null) {
+//                    continue;
+//                }
+//
+//                Song song = new Song(songId, songName, songPath, songImagePath, songDuration,
+//                        albumId, songLyrics, songYear, songDate, songLanguage);
+//                songs.add(song);
+//            }
+//        }
+//        if (c != null){
+//            c.close();
+//        }
+//        try {
+//            db.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        return songs;
+    }
+
+    @Override
+    public void deleteSongs(@NonNull String albumId) {
+        songDao.deleteSongs(albumId);
+
+//        SupportSQLiteDatabase db = mDbHelper.getWritableDatabase();
+//        String selection = SongEntry.COLUMN_NAME_ALBUM_ID + " LIKE ?";
+//        String[] selectionArgs = {albumId};
+//        db.delete(SongEntry.TABLE_NAME, selection, selectionArgs);
+//        try {
+//            db.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+    }
+
+    @Override
+    public List<Integer> printAllSongs() {
+        return songDao.printAllSongs();
+
+//        SupportSQLiteDatabase db = mDbHelper.getReadableDatabase();
+//
+//        String table = AlbumEntry.TABLE_NAME + " AS ALBUM INNER JOIN " + SongEntry.TABLE_NAME +" AS SONG "+
+//                "ON ALBUM." + AlbumEntry.COLUMN_NAME_ENTRY_ID +" = SONG." + SongEntry.COLUMN_NAME_ALBUM_ID;
+//        String columns[] = {
+//                "ALBUM." + AlbumEntry.COLUMN_NAME_ALBUM_NAME +" AS ALBUM",
+//                "ALBUM." + AlbumEntry.COLUMN_NAME_ALBUM_ARTIST +" AS ARTIST",
+//                "SONG." + SongEntry.COLUMN_NAME_SONG_NAME + " AS NAME",
+//                "SONG." + SongEntry.COLUMN_NAME_SONG_DURATION + " AS DURATION",
+//                "SONG." + SongEntry.COLUMN_NAME_SONG_LYRICS + " AS LYRICS",
+//                "SONG." + SongEntry.COLUMN_NAME_SONG_YEAR + " AS YEAR",
+//                "SONG." + SongEntry.COLUMN_NAME_SONG_DATE + " AS DATE",
+//                "SONG." + SongEntry.COLUMN_NAME_SONG_LANGUAGE + " AS LANGUAGE "
+//        };
+//        SupportSQLiteQuery query = SupportSQLiteQueryBuilder.builder(table)
+//                .columns(columns)
+//                .create();
+//        Cursor c = db.query(query);//db.query(table, columns, null, null, null, null, null);
+//        ArrayList<Integer> durations = Util.logCursor(c);
+//
+//        if (c!= null){
+//            c.close();
+//        }
+//        try {
+//            db.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        return durations;
+    }
+
+    @NonNull
+    @Override

1eb36cfca0be0a1086799650788794fd5864b51c
inv779 <109506977+inv779@users.noreply.github.com>
2021-03-12 13:26:21 +0000

    chore(release): 0.4.0

--- app/src/main/java/com/romankaranchuk/musicplayer/data/db/SQLiteOpenHelperImpl.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/data/db/SQLiteOpenHelperImpl.kt
@@ -1,1 +1,6 @@
-package com.romankaranchuk.musicplayer.
+package com.romankaranchuk.musicplayer.data.db
+
+import android.content.Context
+import android.database.sqlite.SQLiteOpenHelper
+import com.romankaranchuk.musicplayer.data.db.SQLiteOpenHelperImpl
+import android.database.sqlite.SQLite

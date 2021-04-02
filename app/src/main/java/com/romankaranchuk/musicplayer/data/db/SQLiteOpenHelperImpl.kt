ac378c68ccfc9ea0869887c3cf330b3df0985fbd
inv779 <109506977+inv779@users.noreply.github.com>
2021-04-02 14:52:58 +0000

    Concepteur(trice) développeur(se) de système électronique

--- app/src/main/java/com/romankaranchuk/musicplayer/data/db/SQLiteOpenHelperImpl.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/data/db/SQLiteOpenHelperImpl.kt
@@ -67,4 +67,6 @@ class SQLiteOpenHelperImpl internal constructor(
                     SongEntry.COLUMN_NAME_SONG_IMAGE + TEXT_TYPE + COMMA_SEP +
                     SongEntry.COLUMN_NAME_SONG_DURATION + INTEGER_TYPE + COMMA_SEP +
                     SongEntry.COLUMN_NAME_SONG_PATH + TEXT_TYPE + COMMA_SEP +
-                    So
+                    SongEntry.COLUMN_NAME_SONG_LYRICS + TEXT_TYPE + COMMA_SEP +
+                    SongEntry.COLUMN_NAME_SONG_YEAR + TEXT_TYPE + COMMA_SEP +
+                    SongEn

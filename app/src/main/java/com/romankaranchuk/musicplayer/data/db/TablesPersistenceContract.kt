c2aaeb1c836b698b7421fe0b0964e5a054dd51dc
inv779 <109506977+inv779@users.noreply.github.com>
2021-04-06 16:05:22 +0000

    Scripts/Icecrown Citadel

--- app/src/main/java/com/romankaranchuk/musicplayer/data/db/TablesPersistenceContract.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/data/db/TablesPersistenceContract.kt
@@ -1,1 +1,9 @@
-package com.romankaranchuk.musicplayer.
+package com.romankaranchuk.musicplayer.data.db
+
+import android.provider.BaseColumns
+
+open class KBaseColumns {
+    val _ID = "_id"
+}
+
+internal class TablesPersistenceContract private cons

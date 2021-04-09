470a47c26c0163e6242ff3ca12421cc1d76b2b0b
inv779 <109506977+inv779@users.noreply.github.com>
2021-04-09 19:00:07 +0000

    update atom

--- app/src/main/java/com/romankaranchuk/musicplayer/data/db/TablesPersistenceContract.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/data/db/TablesPersistenceContract.kt
@@ -10,4 +10,6 @@ internal class TablesPersistenceContract private constructor() {
     internal class AlbumEntry private constructor(): BaseColumns {
         companion object : KBaseColumns() {
             const val TABLE_NAME = "albums"
-            co
+            const val COLUMN_NAME_ENTRY_ID = "album_id"
+            const val COLUMN_NAME_ALBUM_NAME = "album_name"
+            const val COLUMN_NAME_ALBUM_ARTIST = 

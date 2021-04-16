619f614badd9056aaf64ff9eed818bd15680b007
inv779 <109506977+inv779@users.noreply.github.com>
2021-04-16 22:19:11 +0000

    updated NEWS.

--- app/src/main/java/com/romankaranchuk/musicplayer/di/AppComponent.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/di/AppComponent.kt
@@ -1,3 +1,6 @@
 package com.romankaranchuk.musicplayer.di
 
-import android.content.Con
+import android.content.Context
+import com.romankaranchuk.musicplayer.di.module.AppModule
+import com.romankaranchuk.musicplayer.di.module.DataModule
+import com.romankaranchuk.musicplayer.di.module.UseC

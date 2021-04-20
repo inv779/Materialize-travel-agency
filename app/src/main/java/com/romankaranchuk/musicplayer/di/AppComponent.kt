1e16a4c8caa16ea1f35c8a6b7c41230e1d669263
inv779 <109506977+inv779@users.noreply.github.com>
2021-04-20 13:34:10 +0000

    New hr-local

--- app/src/main/java/com/romankaranchuk/musicplayer/di/AppComponent.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/di/AppComponent.kt
@@ -3,4 +3,7 @@ package com.romankaranchuk.musicplayer.di
 import android.content.Context
 import com.romankaranchuk.musicplayer.di.module.AppModule
 import com.romankaranchuk.musicplayer.di.module.DataModule
-import com.romankaranchuk.musicplayer.di.module.UseC
+import com.romankaranchuk.musicplayer.di.module.UseCaseModule
+import com.romankaranchuk.musicplayer.di.module.ViewModelModule
+import com.romankaranchuk.musicplayer.di.module.injector.ActivityModule
+import com.roman

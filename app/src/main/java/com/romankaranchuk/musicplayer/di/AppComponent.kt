e1713a9298b1c262cae147a7e9baa1abd2572871
inv779 <109506977+inv779@users.noreply.github.com>
2021-04-21 11:12:45 +0000

    Clean up README.

--- app/src/main/java/com/romankaranchuk/musicplayer/di/AppComponent.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/di/AppComponent.kt
@@ -6,4 +6,6 @@ import com.romankaranchuk.musicplayer.di.module.DataModule
 import com.romankaranchuk.musicplayer.di.module.UseCaseModule
 import com.romankaranchuk.musicplayer.di.module.ViewModelModule
 import com.romankaranchuk.musicplayer.di.module.injector.ActivityModule
-import com.roman
+import com.romankaranchuk.musicplayer.di.module.injector.FragmentModule
+import com.romankaranchuk.musicplayer.di.module.injector.ServiceModule
+import com.romankaranchuk.m

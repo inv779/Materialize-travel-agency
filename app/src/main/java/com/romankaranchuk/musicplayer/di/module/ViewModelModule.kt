169fead97992ddc2ad9d60cdf9c7f5e93466a794
inv779 <109506977+inv779@users.noreply.github.com>
2021-05-08 12:20:42 +0000

    Update nerdtree plugin

--- app/src/main/java/com/romankaranchuk/musicplayer/di/module/ViewModelModule.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/di/module/ViewModelModule.kt
@@ -4,4 +4,6 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.romankaranchuk.musicplayer.di.util.ViewModelFactory
 import com.romankaranchuk.musicplayer.di.util.ViewModelKey
-import com.romankaranchu
+import com.romankaranchuk.musicplayer.presentation.ui.main.MainViewModel
+import com.romankaranchuk.musicplayer.presentation.ui.player.PlayerViewModel
+import com.romankaranchuk.musicplayer.presen

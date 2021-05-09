be3431b048a14d0ff0ece2bed89f67e169c02668
inv779 <109506977+inv779@users.noreply.github.com>
2021-05-09 21:15:24 +0000

    Update run.h

--- app/src/main/java/com/romankaranchuk/musicplayer/di/module/ViewModelModule.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/di/module/ViewModelModule.kt
@@ -8,4 +8,6 @@ import com.romankaranchuk.musicplayer.presentation.ui.main.MainViewModel
 import com.romankaranchuk.musicplayer.presentation.ui.player.PlayerViewModel
 import com.romankaranchuk.musicplayer.presentation.ui.player.sleeptimer.SleepTimerViewModel
 import com.romankaranchuk.musicplayer.presentation.ui.player.actions.SongActionsViewModel
-import com.romankaranchu
+import com.romankaranchuk.musicplayer.presentation.ui.player.lyrics.SongLyricsViewModel
+import com.romankaranchuk.musicplayer.presentation.ui.player.page.PlayerPageViewModel
+import com.romankar

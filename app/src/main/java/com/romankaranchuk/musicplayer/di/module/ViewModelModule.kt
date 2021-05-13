1db4cefdf1c7542ef95eab19aa895c1d34995647
inv779 <109506977+inv779@users.noreply.github.com>
2021-05-13 10:31:14 +0000

    Fix data

--- app/src/main/java/com/romankaranchuk/musicplayer/di/module/ViewModelModule.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/di/module/ViewModelModule.kt
@@ -15,4 +15,11 @@ import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
 
-@Suppress("unuse
+@Suppress("unused")
+@Module
+abstract class ViewModelModule {
+
+    @Binds
+    abstract fun bindViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
+
+    @Bi

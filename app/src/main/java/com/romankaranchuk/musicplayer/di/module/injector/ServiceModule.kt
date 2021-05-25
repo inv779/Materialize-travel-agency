f09a439a3e6a39197b2fa91a17d30b25bceef3b5
inv779 <109506977+inv779@users.noreply.github.com>
2021-05-25 16:09:02 +0000

    Update task.h

--- app/src/main/java/com/romankaranchuk/musicplayer/di/module/injector/ServiceModule.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/di/module/injector/ServiceModule.kt
@@ -1,1 +1,4 @@
-package com.romankaranchuk.
+package com.romankaranchuk.musicplayer.di.module.injector
+
+import com.romankaranchuk.musicplayer.presentation.service.PlayerService
+import com.romankaranchuk.musicplayer.presentation.service.Sear

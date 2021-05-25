ccf8729d4f3f06ca2c02c329acfa1f62e14b18ac
inv779 <109506977+inv779@users.noreply.github.com>
2021-05-25 21:43:17 +0000

    language support

--- app/src/main/java/com/romankaranchuk/musicplayer/di/module/injector/ServiceModule.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/di/module/injector/ServiceModule.kt
@@ -1,4 +1,12 @@
 package com.romankaranchuk.musicplayer.di.module.injector
 
 import com.romankaranchuk.musicplayer.presentation.service.PlayerService
-import com.romankaranchuk.musicplayer.presentation.service.Sear
+import com.romankaranchuk.musicplayer.presentation.service.SearchService
+import dagger.Module
+import dagger.android.ContributesAndroidInjector
+
+@Module
+abstract class ServiceModule {
+
+    @ContributesAndroidInjector
+    abstract fun cont

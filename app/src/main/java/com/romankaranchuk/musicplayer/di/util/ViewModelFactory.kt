99517afc58064a4bd7d7327680e05b1fb5260977
inv779 <109506977+inv779@users.noreply.github.com>
2021-05-28 12:17:54 +0000

    version 2.0.0

--- app/src/main/java/com/romankaranchuk/musicplayer/di/util/ViewModelFactory.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/di/util/ViewModelFactory.kt
@@ -1,4 +1,11 @@
 package com.romankaranchuk.musicplayer.di.util
 
 import androidx.lifecycle.ViewModel
-import androidx.life
+import androidx.lifecycle.ViewModelProvider
+import javax.inject.Inject
+import javax.inject.Provider
+import javax.inject.Singleton
+
+@Suppress("UNCHECKED_CAST")
+@Singleton
+class ViewModelF

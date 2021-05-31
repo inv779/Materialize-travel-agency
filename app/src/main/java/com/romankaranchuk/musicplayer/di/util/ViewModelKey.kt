beb487fc992414922bf997abdf95e1a234ba12d2
inv779 <109506977+inv779@users.noreply.github.com>
2021-05-31 16:22:05 +0000

    Release 1.4

--- app/src/main/java/com/romankaranchuk/musicplayer/di/util/ViewModelKey.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/di/util/ViewModelKey.kt
@@ -5,4 +5,10 @@ import dagger.MapKey
 import kotlin.reflect.KClass
 
 @Target(
-    Annota
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.PROPERTY_GETTER,
+    AnnotationTarget.PROPERTY_SETTER
+)
+@Retention(AnnotationRetention.RUNTIME)
+@MapKey
+annotation class 

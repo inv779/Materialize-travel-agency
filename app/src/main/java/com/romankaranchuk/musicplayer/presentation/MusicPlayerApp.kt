c54eadcac4890edf128086ef81eb1a1474876517
inv779 <109506977+inv779@users.noreply.github.com>
2021-06-05 09:12:10 +0000

    Fixes #107

--- app/src/main/java/com/romankaranchuk/musicplayer/presentation/MusicPlayerApp.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/presentation/MusicPlayerApp.kt
@@ -3,4 +3,8 @@ package com.romankaranchuk.musicplayer.presentation
 import android.app.Activity
 import android.app.Application
 import android.content.Context
-import android.o
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import com.romankaranchuk.musicpl

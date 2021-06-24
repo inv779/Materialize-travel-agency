8afba0a6f8bcea4ad468418e4df58fa0fcc2a47f
inv779 <109506977+inv779@users.noreply.github.com>
2021-06-24 07:40:52 +0000

    chore: update demo

--- app/src/main/java/com/romankaranchuk/musicplayer/presentation/navigation/Navigator.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/presentation/navigation/Navigator.kt
@@ -2,4 +2,7 @@ package com.romankaranchuk.musicplayer.presentation.navigation
 
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
-import com.romankaranc
+import com.romankaranchuk.musicplayer.R
+import com.romankaranchuk.musicplayer.data.Song
+import com.romankaranchuk.musicplayer.presentation.ui.main.MainFragment
+im

278cd41554cee22b9687d962c25bc59d40167142
inv779 <109506977+inv779@users.noreply.github.com>
2021-06-25 13:14:55 +0000

    Fix coverity reports (dereference null value)

--- app/src/main/java/com/romankaranchuk/musicplayer/presentation/navigation/Navigator.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/presentation/navigation/Navigator.kt
@@ -5,4 +5,5 @@ import androidx.fragment.app.FragmentActivity
 import com.romankaranchuk.musicplayer.R
 import com.romankaranchuk.musicplayer.data.Song
 import com.romankaranchuk.musicplayer.presentation.ui.main.MainFragment
-im
+import com.romankaranchuk.musicplayer.presentation.ui.player.actions.SongActionsBottomSheetDialog
+import com.romankaranchuk.musicplayer.presentation.ui.

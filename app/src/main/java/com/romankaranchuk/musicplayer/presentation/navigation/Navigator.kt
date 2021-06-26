525931ad79d3f396e1a35ada8610a8820752a0e3
inv779 <109506977+inv779@users.noreply.github.com>
2021-06-26 20:56:55 +0000

    update changes.xtp

--- app/src/main/java/com/romankaranchuk/musicplayer/presentation/navigation/Navigator.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/presentation/navigation/Navigator.kt
@@ -6,4 +6,6 @@ import com.romankaranchuk.musicplayer.R
 import com.romankaranchuk.musicplayer.data.Song
 import com.romankaranchuk.musicplayer.presentation.ui.main.MainFragment
 import com.romankaranchuk.musicplayer.presentation.ui.player.actions.SongActionsBottomSheetDialog
-import com.romankaranchuk.musicplayer.presentation.ui.
+import com.romankaranchuk.musicplayer.presentation.ui.player.PlayerFragment
+import com.romankaranchuk.musicplayer.presentation.ui.player.sleeptimer.SleepTimerBottomSheetDialog
+import com.romankaranchuk.musicplayer.present

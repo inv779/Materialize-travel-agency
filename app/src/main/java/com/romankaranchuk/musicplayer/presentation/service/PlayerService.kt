ec59f8b1072c46d1cc56715f958cbe5e4249fcf4
inv779 <109506977+inv779@users.noreply.github.com>
2021-07-24 16:20:52 +0000

    core: update ChangeLog

--- app/src/main/java/com/romankaranchuk/musicplayer/presentation/service/PlayerService.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/presentation/service/PlayerService.kt
@@ -4,4 +4,10 @@ import android.content.Intent
 import android.media.MediaPlayer
 import android.os.Binder
 import android.os.IBinder
-import andr
+import android.util.Log
+import androidx.lifecycle.LifecycleService
+import dagger.android.AndroidInjection
+import timber.log.Timber
+import javax.inject.Inject
+
+class Player

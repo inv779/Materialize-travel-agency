38e2fd151fe19b62c92718bd1d1a5076b256d37a
inv779 <109506977+inv779@users.noreply.github.com>
2021-08-03 21:29:44 +0000

    Update contact details

--- app/src/main/java/com/romankaranchuk/musicplayer/presentation/service/SearchService.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/presentation/service/SearchService.kt
@@ -4,4 +4,7 @@ import android.content.Intent
 import android.os.Binder
 import android.os.IBinder
 import android.util.Log
-import androidx.lifecycle.LifecycleS
+import androidx.lifecycle.LifecycleService
+import androidx.lifecycle.ViewModelProvider
+import com.romankaranchuk.musicplayer.presentation.ui.tracklist.TrackListFragment
+import dagger.android.AndroidI

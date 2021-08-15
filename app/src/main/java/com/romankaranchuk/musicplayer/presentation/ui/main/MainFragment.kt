78a8b066689f65510405579599792dd9382dde2c
inv779 <109506977+inv779@users.noreply.github.com>
2021-08-15 12:37:38 +0000

    Improve variable names

--- app/src/main/java/com/romankaranchuk/musicplayer/presentation/ui/main/MainFragment.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/presentation/ui/main/MainFragment.kt
@@ -5,4 +5,8 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.os.Bundle
 import android.view.View
-import androidx.constraintlayout.motion.widget.Moti
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
+import com.romanka

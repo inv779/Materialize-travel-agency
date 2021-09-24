9552162221ab3bf80daa2e8d634c81091bb94368
inv779 <109506977+inv779@users.noreply.github.com>
2021-09-24 18:19:04 +0000

    chore(package): update ava to version 0.20.0

--- app/src/main/java/com/romankaranchuk/musicplayer/presentation/ui/player/PlayerViewModel.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/presentation/ui/player/PlayerViewModel.kt
@@ -3,4 +3,9 @@ package com.romankaranchuk.musicplayer.presentation.ui.player
 import android.content.Context
 import android.media.MediaPlayer
 import android.os.Handler
-import andr
+import android.os.IBinder
+import android.os.Looper
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.*
+import com.romank

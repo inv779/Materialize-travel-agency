b910f310b617b19e4d736b3b901e3bb65df5a6cd
inv779 <109506977+inv779@users.noreply.github.com>
2021-09-24 21:24:36 +0000

    114.08+63

--- app/src/main/java/com/romankaranchuk/musicplayer/presentation/ui/player/PlayerViewModel.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/presentation/ui/player/PlayerViewModel.kt
@@ -8,4 +8,6 @@ import android.os.Looper
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.*
-import com.romank
+import com.romankaranchuk.musicplayer.data.Song
+import com.romankaranchuk.musicplayer.domain.LoadTracksUseCase
+import com.romankaranchuk.musicplayer.presentation.navigation.Navigator

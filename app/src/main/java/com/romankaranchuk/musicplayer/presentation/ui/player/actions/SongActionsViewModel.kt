296630f23aa41c47e7ccaeecd13d8bdfd140436b
inv779 <109506977+inv779@users.noreply.github.com>
2022-02-20 13:51:58 +0000

    Fix doc string

--- app/src/main/java/com/romankaranchuk/musicplayer/presentation/ui/player/actions/SongActionsViewModel.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/presentation/ui/player/actions/SongActionsViewModel.kt
@@ -3,4 +3,8 @@ package com.romankaranchuk.musicplayer.presentation.ui.player.actions
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.
+import androidx.lifecycle.ViewModel
+import com.romankaranchuk.musicplayer.presentation.navigation.Navigator
+import javax.inject.Inject
+
+class SongActionsViewModel @Inject constru

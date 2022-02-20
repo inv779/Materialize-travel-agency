91a5886d2338b43bfa3342be3388a8ba684efa2e
inv779 <109506977+inv779@users.noreply.github.com>
2022-02-20 18:42:37 +0000

    Added basic README

--- app/src/main/java/com/romankaranchuk/musicplayer/presentation/ui/player/actions/SongActionsViewModel.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/presentation/ui/player/actions/SongActionsViewModel.kt
@@ -7,4 +7,10 @@ import androidx.lifecycle.ViewModel
 import com.romankaranchuk.musicplayer.presentation.navigation.Navigator
 import javax.inject.Inject
 
-class SongActionsViewModel @Inject constru
+class SongActionsViewModel @Inject constructor(
+    private val navigator: Navigator
+) : ViewModel(), DefaultLifecycleObserver {
+
+    private var songId = "-1"
+
+    override fun onCreate(owner: LifecycleO

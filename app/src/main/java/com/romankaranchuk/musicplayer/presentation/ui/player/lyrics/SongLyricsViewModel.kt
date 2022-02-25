a8b446b3cad110f89e0b0da695aad71fff494aed
inv779 <109506977+inv779@users.noreply.github.com>
2022-02-25 07:15:59 +0000

    normalize card script

--- app/src/main/java/com/romankaranchuk/musicplayer/presentation/ui/player/lyrics/SongLyricsViewModel.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/presentation/ui/player/lyrics/SongLyricsViewModel.kt
@@ -4,4 +4,7 @@ import android.content.Context
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.DefaultLifecycleObserver
-imp
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ViewModel
+import com.romankaranchuk.musicplayer.domain.LoadTracksUseCase
+import kotlinx.

59d4458a7f8cb92fee071e74bbd2a8a846878215
inv779 <109506977+inv779@users.noreply.github.com>
2022-03-08 14:04:13 +0000

    ciao

--- app/src/main/java/com/romankaranchuk/musicplayer/presentation/ui/tracklist/edit/EditAudioFragment.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/presentation/ui/tracklist/edit/EditAudioFragment.kt
@@ -1,1 +1,6 @@
-package com.romankaranc
+package com.romankaranchuk.musicplayer.presentation.ui.tracklist.edit
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view

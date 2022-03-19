a63f2696a2a12e50f0abf8295b404bbf01c743c6
inv779 <109506977+inv779@users.noreply.github.com>
2022-03-19 20:09:29 +0000

    normal

--- app/src/main/java/com/romankaranchuk/musicplayer/presentation/ui/tracklist/edit/EditAudioFragment.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/presentation/ui/tracklist/edit/EditAudioFragment.kt
@@ -77,4 +77,6 @@ class EditAudioFragment : DialogFragment() {
             //                }
             //                selectedSong.getAlbumId();
             dismiss()
-    
+        }
+    }
+}
---
+++ app/src/main/java/com/romankaranchuk/musicplayer/utils/JniUtils.java
@@ -0,0 +1,5 @@
+package com.romankaranchuk.musicplayer.utils;
+
+import static com.romankaranchuk.musicplayer.presentation.ui.main.MainActivity.path;
+
+import com.romankaranch

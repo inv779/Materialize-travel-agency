3b08bca9b3d5cb497edd1e13171e40bfc53fdd07
inv779 <109506977+inv779@users.noreply.github.com>
2022-03-19 15:27:03 +0000

    readMe

--- app/src/main/java/com/romankaranchuk/musicplayer/presentation/ui/tracklist/edit/EditAudioFragment.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/presentation/ui/tracklist/edit/EditAudioFragment.kt
@@ -70,4 +70,6 @@ class EditAudioFragment : DialogFragment() {
         binding.editOk.setOnClickListener {
             //                selectedSong.setName(song_name.getText().toString());
             //                ArrayList<Song> list = TrackListFragment.getSongs();
-            //
+            //                for(int i = 0; i < list.size(); i++){
+            //                    if (list.get(i).equals(selectedSong)){
+            //                  

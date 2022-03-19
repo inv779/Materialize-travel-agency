707db7f8eb5ff81d47e35db52165c2db0e14ebcb
inv779 <109506977+inv779@users.noreply.github.com>
2022-03-19 20:36:29 +0000

    [MERGE]sync with trunk

--- app/src/main/java/com/romankaranchuk/musicplayer/utils/JniUtils.java
+++ app/src/main/java/com/romankaranchuk/musicplayer/utils/JniUtils.java
@@ -2,4 +2,12 @@ package com.romankaranchuk.musicplayer.utils;
 
 import static com.romankaranchuk.musicplayer.presentation.ui.main.MainActivity.path;
 
-import com.romankaranch
+import com.romankaranchuk.musicplayer.data.Song;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import timber.log.Timber;
+
+public class

e280469f973b1216a4cb5bbd3b8564c74dba45bc
inv779 <109506977+inv779@users.noreply.github.com>
2022-03-31 06:54:10 +0000

    content update

--- app/src/main/java/com/romankaranchuk/musicplayer/utils/MusicUtils.java
+++ app/src/main/java/com/romankaranchuk/musicplayer/utils/MusicUtils.java
@@ -7,3 +7,11 @@ import com.romankaranchuk.musicplayer.data.Song;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.LinkedList;
+import java.util.List;
+
+
+
+public final class MusicUtils {
+
+    private static final String mCovers[] = {
+            "https://lastfm-img2.akamaized.net/i/u/ar0

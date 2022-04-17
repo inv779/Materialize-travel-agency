c5c4643c11936a90c4fbb2d6c828636ff9bb50b9
inv779 <109506977+inv779@users.noreply.github.com>
2022-04-17 10:04:54 +0000

    Version Inicial

--- app/src/main/java/com/romankaranchuk/musicplayer/utils/Utils.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/utils/Utils.kt
@@ -5,4 +5,9 @@ import android.util.DisplayMetrics
 import android.view.View
 import android.view.Window
 import android.view.WindowManager
-import andr
+import android.view.animation.AlphaAnimation
+import androidx.core.content.ContextCompat
+
+fun hideSystemUI(window: Window) {
+    // Enables regular immersive mode.
+    

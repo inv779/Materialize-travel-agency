45e8029dee649a7c8efa1ae00a462943a7850271
inv779 <109506977+inv779@users.noreply.github.com>
2022-04-18 11:48:03 +0000

    Improved doc

--- app/src/main/java/com/romankaranchuk/musicplayer/utils/Utils.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/utils/Utils.kt
@@ -13,4 +13,7 @@ fun hideSystemUI(window: Window) {
     // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
     // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
     window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
-            // Set the content to appear under the system bars 
+            // Set the content to appear under the system bars so that the
+            // content doesn't resize when the system bars hide and show.
+            or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+      

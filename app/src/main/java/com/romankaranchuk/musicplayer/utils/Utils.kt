b94729fb1ec34bd831614cd1dbe11dafe781fbc4
inv779 <109506977+inv779@users.noreply.github.com>
2022-04-18 13:23:43 +0000

    Updating deps.

--- app/src/main/java/com/romankaranchuk/musicplayer/utils/Utils.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/utils/Utils.kt
@@ -16,4 +16,6 @@ fun hideSystemUI(window: Window) {
             // Set the content to appear under the system bars so that the
             // content doesn't resize when the system bars hide and show.
             or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-      
+            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            // Hide the nav bar a

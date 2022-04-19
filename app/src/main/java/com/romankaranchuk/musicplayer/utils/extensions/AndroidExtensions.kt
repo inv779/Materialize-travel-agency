872d6fae33972eeb1d1396b22a35983eaa8adc20
inv779 <109506977+inv779@users.noreply.github.com>
2022-04-19 12:26:43 +0000

    initial checkin

--- app/src/main/java/com/romankaranchuk/musicplayer/utils/extensions/AndroidExtensions.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/utils/extensions/AndroidExtensions.kt
@@ -2,4 +2,8 @@ package com.romankaranchuk.musicplayer.utils
 
 import android.view.View
 import android.widget.FrameLayout
-import com.google.android.material.bottomsheet
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.romankaranchuk.musicplayer.R
+
+fun com.google.android.material.bottomsheet.BottomSheetDialog.setExpandToFullHeightBehavior(
+    isCa

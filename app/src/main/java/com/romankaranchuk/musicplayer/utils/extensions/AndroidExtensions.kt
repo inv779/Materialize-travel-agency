5299ffe77ef6c9bf0750f6b1d3f4a643c586347a
inv779 <109506977+inv779@users.noreply.github.com>
2022-04-20 21:36:57 +0000

    fixed testdata

--- app/src/main/java/com/romankaranchuk/musicplayer/utils/extensions/AndroidExtensions.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/utils/extensions/AndroidExtensions.kt
@@ -6,4 +6,7 @@ import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.romankaranchuk.musicplayer.R
 
 fun com.google.android.material.bottomsheet.BottomSheetDialog.setExpandToFullHeightBehavior(
-    isCa
+    isCancellableByDragging: Boolean = true
+) {
+    val bottomSheet = this.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet) as FrameLayout
+    val behavi

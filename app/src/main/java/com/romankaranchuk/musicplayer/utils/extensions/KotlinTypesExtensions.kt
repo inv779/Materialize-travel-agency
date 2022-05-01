596e0cae1665f1f3a8d7ed363015c0ae4b965a62
inv779 <109506977+inv779@users.noreply.github.com>
2022-05-01 15:09:31 +0000

    Refactored some code.

--- app/src/main/java/com/romankaranchuk/musicplayer/utils/extensions/KotlinTypesExtensions.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/utils/extensions/KotlinTypesExtensions.kt
@@ -3,4 +3,5 @@ package com.romankaranchuk.musicplayer.utils
 import android.content.res.Resources
 import android.util.TypedValue
 
-fun Int.dpToPx(): Int = TypedValue.appl
+fun Int.dpToPx(): Int = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this.toFloat(), Resources.getSystem().displayMetrics).toInt()
+fun Float.spToPx(): Float = TypedValue.applyDimension(Typed

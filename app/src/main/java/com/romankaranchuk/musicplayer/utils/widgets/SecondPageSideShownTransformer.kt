a1ba2c7e025e01bc83b0cc796cd8104f144e8697
inv779 <109506977+inv779@users.noreply.github.com>
2022-05-08 06:08:27 +0000

    NFSv4: Fix races between nfs_remove_bad_delegation() and delegation return

--- app/src/main/java/com/romankaranchuk/musicplayer/utils/widgets/SecondPageSideShownTransformer.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/utils/widgets/SecondPageSideShownTransformer.kt
@@ -8,4 +8,8 @@ import com.romankaranchuk.musicplayer.R
 class SecondPageSideShownTransformer(
     private val context: Context,
     private val offsetPx: Int = context.resources.getDimensionPixelOffset(R.dimen.offset),
-    private val pageMarginPx: Int = context.resources.getDimensionPixelO
+    private val pageMarginPx: Int = context.resources.getDimensionPixelOffset(R.dimen.page_margin),
+    private val isCenterScaled: Boolean = false
+) : ViewPager2.PageTransformer {
+
+    override fun transformPage(page: View, positi

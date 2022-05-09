72e5ce4b4f8de313c83a6e8953fb5b90ec5864e7
inv779 <109506977+inv779@users.noreply.github.com>
2022-05-09 07:44:12 +0000

    Update for.md

--- app/src/main/java/com/romankaranchuk/musicplayer/utils/widgets/SecondPageSideShownTransformer.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/utils/widgets/SecondPageSideShownTransformer.kt
@@ -16,4 +16,9 @@ class SecondPageSideShownTransformer(
 //        val viewPager = page.parent.parent as ViewPager2
         val offset = position * -(2 * offsetPx + pageMarginPx)
 //        if (viewPager.orientation == ViewPager2.ORIENTATION_HORIZONTAL) {
-//            if (ViewCompat.getLayoutDirection(viewPager) == ViewCompat.LAYOUT_DIRECT
+//            if (ViewCompat.getLayoutDirection(viewPager) == ViewCompat.LAYOUT_DIRECTION_RTL) {
+//                page.translationX = -offset
+//            } else {
+                page.translationX = offset
+//            }
+//      

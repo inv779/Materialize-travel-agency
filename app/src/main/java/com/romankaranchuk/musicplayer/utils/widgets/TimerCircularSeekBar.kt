3ace64f06d2b2b2a399fc218eb2d6d39a1a654fa
inv779 <109506977+inv779@users.noreply.github.com>
2022-05-11 20:23:05 +0000

    Update bundled styles

--- app/src/main/java/com/romankaranchuk/musicplayer/utils/widgets/TimerCircularSeekBar.kt
+++ app/src/main/java/com/romankaranchuk/musicplayer/utils/widgets/TimerCircularSeekBar.kt
@@ -2,4 +2,8 @@ package com.romankaranchuk.musicplayer.utils.widgets
 
 import android.content.Context
 import android.graphics.Canvas
-import andr
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.F
